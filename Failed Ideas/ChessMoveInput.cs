8554f0f50f086dbfe311d0fd304b14cd16ea290f
Enginaaron <165845646+Enginaaron@users.noreply.github.com>
2024-06-19 13:02:36 -0400

    Add files via upload

---
+++ Failed Ideas/ChessMoveInput.cs
@@ -0,0 +1,74 @@
+﻿using System.Diagnostics;
+
+namespace Chess_Input
+{
+    public class Program
+    {
+
+        public static string moveInput()
+        {
+            int move;
+            string rawInput = "";
+            bool error;
+            int fromPosLetter, toPosLetter;
+            do
+            {
+                error = false;
+                Console.WriteLine("Enter Move as location of piece you want to move and then the location you want to move it to.\nEx.\tyou want to move a piece from A1 to B3 you would type that as \"A1B3\"\n\n\n");
+                rawInput = ReadString("Please enter your move:\t");
+                fromPosLetter = PosProcessing(rawInput, 0);
+                toPosLetter = PosProcessing(rawInput, 2);
+                if (rawInput == "")
+                {
+                    Console.WriteLine("\nPlease enter a value.");
+                    error = true;
+                } else if (int.TryParse(rawInput.Substring(1, 1), out int trash) == false || int.TryParse(rawInput.Substring(3, 1), out int trash1) == false || int.Parse(rawInput.Substring(1, 1)) > 8 || int.Parse(rawInput.Substring(1, 1)) < 1 || int.Parse(rawInput.Substring(3, 1)) > 8 || int.Parse(rawInput.Substring(3, 1)) < 1 || fromPosLetter == 10 || toPosLetter == 10 || rawInput.Length != 4)
+                {
+                    Console.WriteLine("Ensure move is entered correctly\n\n");
+                    error = true;
+                }
+            } while (error == true);
+
+            int fromPosNum = int.Parse(rawInput.Substring(1, 1));
+            int toPosNum = int.Parse(rawInput.Substring(3, 1));
+            int fromPos = (fromPosLetter * 10) + fromPosNum;
+            int toPos = (toPosLetter * 10) + toPosNum;
+            move = (fromPos * 100) + toPos;
+
+            return rawInput.ToString();
+        }
+
+        public static string ReadString(string prompt)
+        {
+            Console.Write(prompt);
+            return Console.ReadLine();
+        }
+
+        static int PosProcessing(string raw, int picker) 
+        {
+                string Letter = raw.Substring(picker, 1);
+                Letter = Letter.ToUpper();
+                switch (Letter) 
+                {
+                    case "A":
+                        return 1;
+                    case "B":
+                        return 2;
+                    case "C":
+                        return 3;
+                    case "D": 
+                        return 4;
+                    case "E":
+                        return 5;
+                    case "F":
+                        return 6;
+                    case "G":
+                        return 7;
+                    case "H":
+                        return 8;
+                    default:
+                        return 10;
+                }
+        }
+    }
+}
---
+++ Failed Ideas/communicator.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.IO;
+using Chess_Input;
+
+class Communicator : Program
+{
+    static Random rd = new Random(); // initialize rng
+    public static void Main()
+    {
+        // function to handle and verify integer inputs
+        static int ReadInt(int min = int.MinValue, int max = int.MaxValue, int userInt = 0)
+        {
+            while (true)
+            {
+                bool isValid = int.TryParse(Console.ReadLine(), out userInt);
+
+                if (isValid == false) // if input is not an integer
+                {
+                    Console.WriteLine("Please input a valid integer.");
+                }
+                else if (userInt < min || userInt > max) // if input is too little or too large
+                {
+                    Console.WriteLine("Out of range, please input a valid number.");
+                }
+                else { break; } // if input is valid
+            }
+
+            return userInt;
+        }
+
+        static string colourSelect()
+        {
+
+            Console.WriteLine("Enter the value for the piece colour you want to play as:\n1 = White\n2 = Black\n3 = Random");
+            int colour = ReadInt(1, 3);
+            switch (colour)
+            {
+                case 1:
+                    return "White";
+
+                case 2:
+                    return "Black";
+
+                case 3:
+                    int draw = rd.Next(1, 2);
+                    if (draw == 1)
+                    {
+                        return "White";
+                    }
+                    else
+                    {
+                        return "Black";
+                    }
+                default:
+                    return "ERROR: Invalid Input";
+            }
+
+        }
+
+        static int eloSelect()
+        {
+            int elo;
+            while (true)
+            {
+                Console.WriteLine("Enter your current Chess ELO (0 to 3000): ");
+                string tempELO = Console.ReadLine();
+                bool result = int.TryParse(tempELO, out elo);
+    
+                if (result == false || tempELO.Contains('-') || elo <= 0 || elo > 3000)
+                {
+                    Console.WriteLine("Please enter a valid integer from 0 to 3000.");
+                }
+                else
+                {
+                    return elo;
+                }
+            }
+        }
+
+        // gets user's elo and uses it to calculate bot elo
+        static int difficultySelect()
+        {
+            Console.WriteLine("Enter your desired difficulty\n1 = Easy\n2 = Medium\n3 = Hard\n4 = NIGHTMARE...");
+            int difficulty = ReadInt(1, 4);
+    
+            switch (difficulty)
+            {
+                case 1:
+                    return eloSelect() / 2;
+    
+                case 2:
+                    return eloSelect();
+    
+                case 3:
+                    return eloSelect() * 2;
+    
+                case 4:
+                    return 3700;
+    
+                default:
+                    return 0;
+            }
+        }
+
+        // store values in variables
+        string colour = colourSelect();
+        string botELO = difficultySelect().ToString();
+        string playerMove = moveInput();
+
+        // write to file
+        File.WriteAllText("communication.txt", colour);
+        File.AppendAllText("communication.txt", $" {botELO}");
+        File.AppendAllText("communication.txt", $" {playerMove}");
+        }
+    }
