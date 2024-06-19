e47a88206e9f9cb74cf23a455cd53b6176fa741c
Aarav Kohli <91758898+AK2640@users.noreply.github.com>
2024-06-19 16:18:34 -0400

    Add files via upload

---
+++ game.py
@@ -0,0 +1,87 @@
+# Written By: Aarav Kohli with Mitchel Reigert, and Aaron Dam
+# Course: ICS3U
+# Date: 6/19/2024
+# Program Description: This program takes in the input parameters
+# From the JSON File, and Plays Chest Against the User in a 
+# GUI
+
+
+# Imports
+from tkinter import *
+import json
+from stockfish import Stockfish
+
+stockfish = Stockfish(path=r"./stockfish-windows-x86-64-avx2.exe") # Intialize Stockfish
+currentMoves = [] # Array For Moves
+stringToGiveUser = stockfish.get_board_visual() # Set Base Chess Board
+Font_tuple = ("Courier New", 20, "bold") # Font settings
+
+
+def playMovesWhite():
+    currentMoves.append(moveEntry.get()) # Append the User's move to the list
+    stockfish.set_position(currentMoves) # Update Stockfish Position
+    currentMoves.append(stockfish.get_best_move()) # Add Stockfish's move
+    stockfish.set_position(currentMoves) # Update Stockfish Position
+    stringToGiveUser = stockfish.get_board_visual() # Set the Board visual (string)
+    computerText.configure(text=stringToGiveUser, font=Font_tuple) # Change the text on the GUI
+
+def playMovesBlack():
+    currentMoves.append(moveEntry.get()) # Append the User's move to the list
+    stringToGiveUser = stockfish.get_board_visual() # Set the Board visual (string)
+    computerText.configure(text=stringToGiveUser, font=Font_tuple) # Change the text on the GUI
+    stockfish.set_position(currentMoves) # Update Stockfish Position
+    currentMoves.append(stockfish.get_best_move()) # Add Stockfish's move
+    stockfish.set_position(currentMoves) # Update the stockfish position
+    stringToGiveUser = stockfish.get_board_visual()  # Set the Board visual (string)
+    computerText.configure(text=stringToGiveUser, font=Font_tuple) # Change the text on the GUI
+
+colour = 1 # Define colour
+
+# Opening JSON file
+with open('settings.json', 'r') as openfile:
+ 
+    # Reading from json file
+    json_object = json.load(openfile)
+    colour = json_object["Colour"]
+    stockfish.set_elo_rating(json_object["Elo"])
+
+# Define Tkinter Root
+root = Tk()
+root.title('MRADAK Engine')
+
+# Make the Title
+w = Label(root, text='MRADAK Engine')
+w.pack(anchor='w', padx=20, pady=10) 
+
+
+# If the User is playing as black
+if colour == 1:
+    instructions = Label(root, text="Enter a Move") # Tell them to enter a move
+    moveEntry = Entry(root) # Make the input box
+    stockfish.set_position(currentMoves) # Change the stockfish position
+    currentMoves.append(stockfish.get_best_move()) # Add Stockfish's move
+    stockfish.set_position(currentMoves) # Update Stockfish Position
+    stringToGiveUser = stockfish.get_board_visual() # Update board string
+    submit = Button(root, text="Submit", height=1, width=5, command=playMovesWhite) # Submit Button
+    computerText = Label(root, text=stringToGiveUser, font=Font_tuple) # Board Visual
+
+    # Pack Widgets
+    instructions.pack(anchor='w', padx=20, pady=10)
+    moveEntry.pack(anchor='w', padx=20, pady=10)
+    submit.pack(anchor='w', padx=20, pady=10)
+    computerText.pack()
+    root.mainloop()
+
+# If the User is playing as white
+if colour == 2:
+    instructions = Label(root, text="Enter a Move")  # Tell them to enter a move
+    moveEntry = Entry(root) # Make the input box
+    submit = Button(root, text="Submit", height=1, width=5, command=playMovesBlack) # Submit Button
+    computerText = Label(root, text=stringToGiveUser) # Board Visual
+
+    # Pack Widgets
+    instructions.pack(anchor='w', padx=20, pady=10)
+    moveEntry.pack(anchor='w', padx=20, pady=10)
+    submit.pack(anchor='w', padx=20, pady=10)
+    computerText.pack()
+    root.mainloop()
---
+++ gui.py
@@ -0,0 +1,166 @@
+# Written By: Aarav Kohli with Mitchell Riegert, and Aaron Dam
+# Course: ICS3U
+# Date: 6/19/2024
+# Program Description: This program sets up 
+# The Stockfish Engine, to Be Played in the Next GUI
+
+# Imports
+from tkinter import *
+import json
+import subprocess
+import random
+
+# Variables
+elo = 1000
+difficulty = 4
+colour = 1
+
+hasEnteredOptions = False # Exists for further revisions in which the entire game is one GUI (Can't change Parameters Midgame)
+move = ""
+
+# Tkinter Initialized
+
+root = Tk()
+root.title('MRADAK Engine')
+
+# Function to Set the Player to White
+def setWhite():
+    global hasEnteredOptions, colour  
+    if not hasEnteredOptions:
+        # Set the colour while the user is still able to
+        colour = 2
+
+
+# Function to Set the Player to Random
+def setRand():
+    global hasEnteredOptions, colour  
+    if not hasEnteredOptions:
+        # Set the colour while the user is still able to
+        colour = random.randint(1, 2) # Random Colour
+
+
+# Function to Set the Difficulty to Easy
+def setEasy():
+    global hasEnteredOptions, difficulty 
+    if not hasEnteredOptions:
+        difficulty = 1 # Set the difficulty while the user is still able to
+
+# Function to Set the Difficulty to Medium
+def setMedium():
+    global hasEnteredOptions, difficulty 
+    if not hasEnteredOptions:
+        difficulty = 2 # Set the difficulty while the user is still able to
+
+# Function to Set the Difficulty to Hard
+def setHard():
+    global hasEnteredOptions, difficulty 
+    if not hasEnteredOptions:
+        difficulty = 3 # Set the difficulty while the user is still able to
+
+# Function to Set the Difficulty to Hardest
+def setNightmare():
+    global hasEnteredOptions, difficulty 
+    if not hasEnteredOptions:
+        difficulty = 4 # Set the difficulty while the user is still able to
+
+# Function to get the User's Elo and set the global variable
+def getElo():
+    global elo
+    elo = int(T.get())
+
+# Function to Lock in the Settings and Close the GUI
+def closeOptions():
+    global hasEnteredOptions, difficulty
+    getElo() # Set Elo
+    hasEnteredOptions = True # Close Setting Selection
+    computerElo = 0 # Create Computer Elo
+
+    root.destroy() # Close the GUI
+
+    # Change the Computer Elo based on teh difficulty
+    if difficulty == 1:
+        computerElo = elo / 2
+    elif difficulty == 2:
+        computerElo = elo
+    elif difficulty == 3:
+         computerElo = elo * 2
+    elif difficulty == 4:
+        computerElo = 4000
+
+    # Create a dictionary for neccessary Data
+    toFile = {
+        "Colour": colour,
+        "Elo": computerElo
+    }
+
+    # Serializing json
+    json_object = json.dumps(toFile, indent=4)
+ 
+    # Writing to json
+    with open("settings.json", "w") as outfile:
+        outfile.write(json_object)
+
+    # Run the Game.py file with subprocess
+    subprocess.run(["python", "game.py"])
+
+
+    
+
+# Title
+w = Label(root, text='MRADAK Engine')
+w.pack(anchor='w', padx=20, pady=10)
+
+# Instructions
+instructions = Label(root, text="Please enter the following information:\n\n")
+instructions.pack(anchor='w', padx=20)
+
+# Colour Heading
+colourText = Label(root, text="What Colour Would you Like to Play As?")
+colourText.pack(anchor='w', padx=20) 
+
+# Black Button
+blackButton = Checkbutton(root, text="Black")
+blackButton.pack(anchor='w', padx=40)
+
+# White Button
+whiteButton = Checkbutton(root, text="White", command=setWhite)
+whiteButton.pack(anchor='w', padx=40) 
+
+# Random button
+randButton = Checkbutton(root, text="Random", command=setRand)
+randButton.pack(anchor='w', padx=40)
+
+# Elo Text
+eloText = Label(root, text="What is your Elo?")
+eloText.pack(anchor='w', padx=20, pady=(10, 0))
+
+# Text Entry for Elo
+T = Entry(root)
+T.pack(anchor='w', padx=40)
+
+# Difficulty Text
+difficultyText = Label(root, text="Select your Difficulty:")
+difficultyText.pack(anchor='w', padx=20)
+
+# Easy Button
+easyButton = Checkbutton(root, text="Easy", command=setEasy)
+easyButton.pack(anchor='w', padx=40)
+
+# Medium Button
+mediumButton = Checkbutton(root, text="Medium", command=setMedium)
+mediumButton.pack(anchor='w', padx=40)
+
+# Hard Button
+hardButton = Checkbutton(root, text="Hard", command=setHard)
+hardButton.pack(anchor='w', padx=40)
+
+# Nightmare Button
+nightmareButton = Checkbutton(root, text="Nightmare", command=setNightmare)
+nightmareButton.pack(anchor='w', padx=40)
+
+# Button to start
+continueButton = Button(root, text="Start", height=1, width=5, command=closeOptions)
+continueButton.pack(anchor='w', padx=20, pady=10) 
+
+
+root.mainloop()
